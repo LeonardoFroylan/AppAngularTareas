fd5a590cddfa4968dab64558c68386d728c2f47c
LeonardoFroylan <l2225010064@guasave.tecnm.mx>
2025-11-22 18:22:32 -0700

    Enhance App component with todo functionality
    Added methods for adding and deleting todos, and updated component imports.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.html', // Enlaza con html
+  styleUrl: './app.css'      // Enlaza con css
 })
 export class App {
-  protected readonly title = signal('app');
+  // Tareas iniciales
+  todoArray: string[] = ['Dominar Angular 20', 'Practicar TypeScript', 'Hacer ejercicio'];
+
+  newTodo: string = '';
+  errorMsg: string = '';
+
+  // Método para agregar una nueva tarea
+  addTodo() {
+    if (this.newTodo.trim() === '') {
+      this.errorMsg = '¡Escribe una tarea primero!';
+      // Borra el mensaje de error después de 2 segundos
+      setTimeout(() => this.errorMsg = '', 2000);
+      return;
+    }
+
+    // la tarea se agrega al inicio de la lista
+    this.todoArray.unshift(this.newTodo);
+    this.newTodo = '';
+    this.errorMsg = '';
+  }
+
+  // Método para eliminar una tarea por su índice
+  deleteItem(index: number) {
+    this.todoArray.splice(index, 1);
+  }
 }
